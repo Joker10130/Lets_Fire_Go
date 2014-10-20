86a3b6871aa3e799803d123205f9c620282ae9cc
mewmeww <mews_nhttmn@hotmail.com>
2014-10-20 20:42:51 +0700

    roll back the code, add attribute and constructor

--- Spreading_Of_Fire/src/spreading_of_fire/View.java
+++ Spreading_Of_Fire/src/spreading_of_fire/View.java
@@ -1,58 +1,17 @@
 package spreading_of_fire;
 
-import java.awt.Color;
-import java.awt.*;
 import javax.swing.*;
 
 /**
  * The view class of project from MVC pattern
- *
- * @version 2014.10.19
+ * 
+ * @author mewmeww
+ * @version 2014.04.19
  */
-public class View extends JPanel  {
-    private final Cell cell[][];
-    private final int blockWidth,blockHeight,step;
-    private final boolean showValue;
-    
-    /**
-     * Paint the cell
-     * @param g
-     */
-    @Override
-    public void paintComponent(Graphics g){
-        //Fill the back ground with Yellow
-        g.setColor(Color.YELLOW);
-        g.fillRect(0, 0, 500, 600);
-        
-        //Calculate the border width
-        int border=(500-(blockWidth*cell.length))/2;
-        
-        //Don't paint if there is no cell
-        if(cell==null)return;
-        for(int i=0;i<cell.length;i++){
-            for(int j=0;j<cell[0].length;j++){
-                //Paint the cell
-                g.setColor(cell[i][j].getColor());
-                g.fillOval(border+j*blockWidth,border+i*blockHeight,blockWidth,blockHeight);
-                
-                //Check the show value or not
-                if(showValue){
-                    //Show the value of each cell
-                    g.setColor(Color.BLACK);
-                    g.setFont(new Font(Font.SANS_SERIF, Font.BOLD, blockHeight));
-                    g.drawString(""+cell[i][j].get(), border+j*blockWidth+blockWidth/4,border+i*blockHeight+blockHeight*7/8);
-                }
-            }
-        }
-        
-        //Paint the "Step" label
-        g.setColor(Color.BLACK);
-        g.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 20));
-        g.drawString("Step: "+step, 10, 550);
-        
-        //Show the showValue toggle status
-        g.drawString("Show Value: "+showValue, 200, 550);
-    }
+public class View  {
+    private Cell cell[][];
+    private int blockWidth,blockHeight,step;
+    private boolean showValue;
     
     /**
      * Constructor - create the view panel
@@ -66,21 +25,38 @@ public class View extends JPanel  {
         cell=null;
     }
     
-    public void toggleValue() {
-        
+    /**
+     * Toggle the showValue
+     */
+    public void toggleValue(){
+        if(showValue)showValue=false;
+        else showValue=true;
     }
-
-    public void setBlockSize(int blockWidth, int blockHeight) {
-        
-    }
-
     
+    /**
+     * Set the block size
+     * @param blockWidth
+     * @param blockHeight
+     */
+    public void setBlockSize(int blockWidth,int blockHeight) {
+        this.blockWidth=blockWidth;
+        this.blockHeight=blockHeight;
+    }
+    
+    /**
+     * Set the step
+     * @param step
+     */
     public void setStep(int step) {
-        
-    }
-
-    public void update(Cell[][] cell) {
-        
+        this.step=step;
     }
     
+    /**
+     * Update the cell
+     * @param cell
+     */
+    public void update(Cell cell[][]){
+        this.cell=cell;
+    }
 }
+
